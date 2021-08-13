90923668955e2e6e5b2c6892bc7ed70824a12a73
Edemkwaku <edenkwaku85@gmail.com>
2021-08-12 19:35:55 -0700

    GPA Application
    A desktop Application for GPA calculation . Ho Technical University

---
+++ gpa_sys/StartUp.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class StartUp
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Guna2GradientPanel1 = New Guna.UI2.WinForms.Guna2GradientPanel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Guna2GradientPanel2 = New Guna.UI2.WinForms.Guna2GradientPanel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ProgressBar = New Guna.UI2.WinForms.Guna2CircleProgressBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Guna2DragControl1 = New Guna.UI2.WinForms.Guna2DragControl(Me.components)
+        Me.Guna2GradientPanel1.SuspendLayout()
+        Me.Guna2GradientPanel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Guna2GradientPanel1
+        '
+        Me.Guna2GradientPanel1.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.Guna2GradientPanel1.Controls.Add(Me.Label1)
+        Me.Guna2GradientPanel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Guna2GradientPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.Guna2GradientPanel1.Name = "Guna2GradientPanel1"
+        Me.Guna2GradientPanel1.ShadowDecoration.Parent = Me.Guna2GradientPanel1
+        Me.Guna2GradientPanel1.Size = New System.Drawing.Size(607, 47)
+        Me.Guna2GradientPanel1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DarkBlue
+        Me.Label1.Location = New System.Drawing.Point(173, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(247, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "HTU GPA CALCULATOR"
+        '
+        'Guna2GradientPanel2
+        '
+        Me.Guna2GradientPanel2.Controls.Add(Me.Label2)
+        Me.Guna2GradientPanel2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Guna2GradientPanel2.FillColor = System.Drawing.Color.RoyalBlue
+        Me.Guna2GradientPanel2.FillColor2 = System.Drawing.Color.CornflowerBlue
+        Me.Guna2GradientPanel2.Location = New System.Drawing.Point(0, 309)
+        Me.Guna2GradientPanel2.Name = "Guna2GradientPanel2"
+        Me.Guna2GradientPanel2.ShadowDecoration.Parent = Me.Guna2GradientPanel2
+        Me.Guna2GradientPanel2.Size = New System.Drawing.Size(607, 29)
+        Me.Guna2GradientPanel2.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold)
+        Me.Label2.ForeColor = System.Drawing.Color.DarkBlue
+        Me.Label2.Location = New System.Drawing.Point(22, 5)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(253, 16)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Copyright© Edem Kwaku Avorley, 2021"
+        '
+        'ProgressBar
+        '
+        Me.ProgressBar.AnimationSpeed = 0.2!
+        Me.ProgressBar.Cursor = System.Windows.Forms.Cursors.WaitCursor
+        Me.ProgressBar.FillThickness = 35
+        Me.ProgressBar.Font = New System.Drawing.Font("Segoe UI Emoji", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ProgressBar.ForeColor = System.Drawing.Color.Violet
+        Me.ProgressBar.Location = New System.Drawing.Point(197, 72)
+        Me.ProgressBar.Name = "ProgressBar"
+        Me.ProgressBar.ProgressColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.ProgressBar.ProgressColor2 = System.Drawing.Color.Yellow
+        Me.ProgressBar.ProgressThickness = 35
+        Me.ProgressBar.ShadowDecoration.BorderRadius = 8
+        Me.ProgressBar.ShadowDecoration.Color = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.ProgressBar.ShadowDecoration.Enabled = True
+        Me.ProgressBar.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle
+        Me.ProgressBar.ShadowDecoration.Parent = Me.ProgressBar
+        Me.ProgressBar.ShowPercentage = True
+        Me.ProgressBar.Size = New System.Drawing.Size(210, 209)
+        Me.ProgressBar.TabIndex = 3
+        '
+        'Timer1
+        '
+        '
+        'Guna2DragControl1
+        '
+        Me.Guna2DragControl1.TargetControl = Me.Guna2GradientPanel1
+        '
+        'StartUp
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.RoyalBlue
+        Me.ClientSize = New System.Drawing.Size(607, 338)
+        Me.Controls.Add(Me.ProgressBar)
+        Me.Controls.Add(Me.Guna2GradientPanel2)
+        Me.Controls.Add(Me.Guna2GradientPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "StartUp"
+        Me.Text = "StartUp"
+        Me.Guna2GradientPanel1.ResumeLayout(False)
+        Me.Guna2GradientPanel1.PerformLayout()
+        Me.Guna2GradientPanel2.ResumeLayout(False)
+        Me.Guna2GradientPanel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Guna2GradientPanel1 As Guna.UI2.WinForms.Guna2GradientPanel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Guna2GradientPanel2 As Guna.UI2.WinForms.Guna2GradientPanel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ProgressBar As Guna.UI2.WinForms.Guna2CircleProgressBar
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Guna2DragControl1 As Guna.UI2.WinForms.Guna2DragControl
+End Class
---
+++ gpa_sys/StartUp.vb
@@ -0,0 +1,17 @@
+﻿
+Public Class StartUp
+
+    Private Sub StartUp_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ProgressBar.Increment(1)
+        If ProgressBar.Value = 100 Then
+            Me.Hide()
+            Level100_1.Show()
+            Timer1.Enabled = False
+        End If
+    End Sub
+
+End Class
